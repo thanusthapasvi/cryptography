bf130bd887c79dd4dedf7da63463903e4094ab1d
thanusthapasvi <123483866+thanusthapasvi@users.noreply.github.com>
2023-11-27 10:16:41 +0530

    Update 02-mono alphabetic cipher.c

--- 02-mono alphabetic cipher.c
+++ 02-mono alphabetic cipher.c
@@ -1,30 +1,28 @@
 #include<stdio.h>
-#include<string.h>
-#include<ctype.h>
 int main()
 {
-	int k,i;
-	char in[50],ci[50];
-	printf("Enter input string : ");
-	scanf("%s",&in);
-	printf("Enter key value (1 to 25) : ");
-	scanf("%d",&k);
-	printf("Encrytption is : ");
-	for(i=0;i<strlen(in);i++)
-	{
-		if(islower(in[i]))
-			ci[i]=((in[i]-'a'+k)%26)+'a';	
-		else
-			ci[i]=((in[i]-'A'+k)%26)+'A';	
-		printf("%c",ci[i]);
+	char alpha[100]="abcdefghijklmnopqrstuvwxyz",key[100]="zyxwvutsrqponmlkjihgfedcba",plain[100],cipher[100];
+	int m=0,index[100],i,j;
+	printf("Enter plain text : ");
+	scanf("%s",&plain);
+	for(i=0;i<strlen(plain);i++){
+		for(j=0;j<strlen(alpha);j++){
+			if(plain[i]==alpha[j]){
+				index[m]=j;
+				m++;
+			}
+		}
 	}
-	printf("\nDecrytption is : ");
-	for(i=0;i<strlen(ci);i++)
+	printf("Cipher text: ");
+	for(i=0;i<strlen(plain);i++)
 	{
-		if(islower(in[i]))
-			in[i]=((ci[i]-'a'-k)%26)+'a';	
-		else
-			in[i]=((ci[i]-'A'-k)%26)+'A';	
-		printf("%c",in[i]);
+		cipher[i]=key[index[i]];
+		printf("%c",cipher[i]);
 	}
+	printf("\nPlain text : ");
+	for(i=0;i<strlen(plain);i++)
+	{
+		plain[i]=alpha[index[i]];
+		printf("%c",plain[i]);
+	}		
 }
